a01ffd95f42d9cd7d163c7f1dc8f1037ff3bb366
Udit-records <111231744+Udit-records@users.noreply.github.com>
2022-08-16 16:57:00 +0530

    Add files via upload

---
+++ Assignment_1.cpp
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    //Ojective_1
+    
+    printf("\nobjective_1\n");
+    printf("Hello");
+    printf("\n\n");
+    
+
+    //Objective_2
+    
+    printf("\nobjective_2\n");
+    printf("Hello");
+    printf("\nStudents");
+    printf("\n\n");
+    
+
+    //objective_3
+    
+    printf("\nobjective_3\n");
+    printf("\"MySirG\"");
+    printf("\n\n");
+    
+
+    //objective_4
+    
+    printf("\nobjective_4\n");
+    float R, A;
+    scanf("%f", &R);
+    A = 3.14 * R * R;
+    printf("Area of circle is %f having the radius %f",A,R);
+    printf("\n\n");
+    
+
+    //objective_5
+    
+    printf("\nobjective_5\n");
+    printf("%d",sizeof("String"));
+    printf("\n\n");
+    
+
+    //objective_6
+    
+    printf("\nobjective_6\n");
+    printf("\"Hello , Amit Kumar\"");
+    printf("\n\n");
+    
+
+    //objective_7
+    
+    printf("\nobjective_7\n");
+    char a='%',b='d';
+    printf("%c%c",a,b);
+    printf("\n");
+    printf("%%");
+    printf("\n\n");
+    
+
+    //objective_8
+    
+    printf("\nobjective_8\n");
+    printf("\\n");
+    printf("\n\n");
+    
+
+    //objective_9
+    
+    printf("\nobjective_9\n");
+    printf("\\\\");
+    printf("\n\n");
+    
+
+    //objective_10
+    
+    printf("\nobjective_10\n");
+    int d,m,y;
+    scanf("%d %d %d",&d, &m, &y);
+    printf("User Input data format-\"%d/%d/%d\"\n",d,m,y);
+    printf("Output format- \"Day-%d, Month-%d,Year-%d\"",d,m,y);
+    printf("\n\n");
+    
+
+    //objective_11
+    
+    printf("\nobjective_11\n");
+    int H,M;
+    scanf("%d %d",&H,&M);
+    printf("User Input time format-\"%d:%d\"\n",H,M);
+    printf("%d Hour and %d Minute",H,M);
+    printf("\n\n");
+    
+
+    //objective_12
+    
+    printf("\nobjective_12\n");
+    int x = printf("ineuron");
+    printf("\n");
+    printf("%d",x);
+    
+
+    return 0;
+
+}
