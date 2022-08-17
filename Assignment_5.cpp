be5e07fb6079fb619a4f8c19ed1286dee3082b45
Udit-records <111231744+Udit-records@users.noreply.github.com>
2022-08-17 22:29:39 +0530

    Add files via upload
    Kindly uncomment each objective/subsection before assessing the same.

---
+++ Assignment_5.cpp
@@ -0,0 +1,171 @@
+#include<stdio.h>
+
+//Kindly uncomment each objective/subsections before assessment 
+
+int main()
+{
+
+    //Objective_1
+    /*
+    printf("\nAssignment_1\n");
+    int i=1,N;
+    printf("Enter the value of N\n");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("%d MySirG\n",i);
+        i++;
+    }
+    */
+    
+
+    
+    //Objective_2
+    /*
+    printf("\nAssignment_2\n");
+    int i=1,N;
+    printf("Enter the value of N\n");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("%d\n",i++);
+    }  
+    */ 
+    
+
+
+    //Objective_3
+    /*
+    printf("\nAssignment_3\n");
+    int N;
+    printf("Enter the value of N\n");
+    scanf("%d",&N);
+    while(N>=1)
+    {
+        printf("%d\n",N--);
+    }
+    */ 
+    
+
+
+    //Objective_4
+    /*
+    printf("\nAssignment_4\n");
+    int i=1,n=1,N;
+    printf("Enter the value of N\n");
+    scanf("%d",&N);
+    while(n<=N)
+    {
+        if (i%2==1)
+        {
+            printf("%d\n",i);
+            n++;
+        }
+        i++;
+    }
+    */
+    
+
+
+    //Objective_5
+    /*
+    printf("\nAssignment_5\n");
+    int i,n=1,N;
+    printf("Enter the value of N\n");
+    scanf("%d",&N);
+    i = 1 + ((N-1)*2);    
+    while(n<=N)
+    {
+        printf("%d\n",i);
+        i = i-2;
+        n++;
+    }
+    */
+    
+    
+
+
+    //Objective_6
+    /*
+    printf("\nAssignment_6\n");
+    int i=1,n=1,N;
+    printf("Enter the value of N\n");
+    scanf("%d",&N);
+    while(n<=N)
+    {
+        if (i%2==0)
+        {
+            printf("%d\n",i);
+            n++;
+        }
+
+        i++;
+    }
+    */
+    
+
+
+    //Objective_7
+    /*
+    printf("\nAssignment_7\n");
+    int i,n=1,N;
+    printf("Enter the value of N\n");
+    scanf("%d",&N);
+    i = 2 + ((N-1)*2);
+    while(n<=N)
+    {
+        printf("%d\n",i);
+        i = i-2;
+        n++;
+    }
+    */
+    
+
+
+    //Objective_8
+    /*
+    printf("\nAssignment_8\n");
+    int i=1,N;
+    printf("Enter the value of N\n");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("%d\n",i*i);
+        i++;
+    }
+    */
+    
+
+
+    //Objective_9
+    /*
+    printf("\nAssignment_9\n");
+    int i=1,N;
+    printf("Enter the value of N\n");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }
+    */
+    
+
+
+    //Objective_10
+    /*
+    printf("\nAssignment_10\n");
+    int i=1,N;
+    printf("Enter the value of N\n");
+    scanf("%d",&N);
+    
+    while(i<=10)
+    {
+        printf("%d\n",N*i);
+        i++;
+    }
+    */
+    
+
+    return 0;
+}
