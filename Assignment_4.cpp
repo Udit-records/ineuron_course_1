c331fca9fdcf77a6a0d596f1ca279312335a3983
Udit-records <111231744+Udit-records@users.noreply.github.com>
2022-08-17 22:01:20 +0530

    Add files via upload
    Kindly uncomment the respective objectives/subsections before assessing the same

---
+++ Assignment_4.cpp
@@ -0,0 +1,138 @@
+#include<stdio.h>
+
+//Kindly uncomment each objective/subsections before assessment 
+
+int main()
+{
+
+    //Objective_1
+    /*
+    printf("\nAssignment_1\n");
+    int i=1;
+    while(i<=5)
+    {
+        printf("%d MySirG\n",i);
+        i++;
+    }
+    */
+
+    
+    //Objective_2
+    /*
+    printf("\nAssignment_2\n");
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\n",i++);
+    }   
+    */
+
+
+    //Objective_3
+    /*
+    printf("\nAssignment_3\n");
+    int i=10;
+    while(i>=1)
+    {
+        printf("%d\n",i--);
+    }   
+    */
+
+
+    //Objective_4
+    /*
+    printf("\nAssignment_4\n");
+    int i=1,n=1;
+    while(n<=10)
+    {
+        if (i%2==1)
+        {
+            printf("%d\n",i);
+            n++;
+        }
+
+        i++;
+    }
+    */
+
+
+    //Objective_5
+    /*
+    printf("\nAssignment_5\n");
+    int i=19,n=1;
+    while(n<=10)
+    {
+        printf("%d\n",i);
+        i = i-2;
+        n++;
+    }
+    */
+
+
+    //Objective_6
+    /*
+    printf("\nAssignment_6\n");
+    int i=1,n=1;
+    while(n<=10)
+    {
+        if (i%2==0)
+        {
+            printf("%d\n",i);
+            n++;
+        }
+
+        i++;
+    }  
+    */
+
+
+    //Objective_7
+    /*
+    printf("\nAssignment_7\n");
+    int i=20,n=1;
+    while(n<=10)
+    {
+        printf("%d\n",i);
+        i = i-2;
+        n++;
+    }
+    */
+
+
+    //Objective_8
+    /*
+    printf("\nAssignment_8\n");
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\n",i*i);
+        i++;
+    } 
+    */
+
+
+    //Objective_9
+    /*
+    printf("\nAssignment_9\n");
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }
+    */
+
+
+    //Objective_10
+    /*
+    printf("\nAssignment_10\n");
+    int i=1;
+    while(i<=10)
+    {
+        printf("%d\n",5*i);
+        i++;
+    }
+    */
+
+    return 0;
+}
