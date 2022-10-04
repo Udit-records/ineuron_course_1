29a699aa060a930d071900892c23f303faab6c72
Udit-records <111231744+Udit-records@users.noreply.github.com>
2022-10-05 01:33:37 +0530

    Add files via upload
    Kindly uncomment the section/objectives before executing them

---
+++ Assignment_23.cpp
@@ -0,0 +1,108 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+//Kindly uncommment the respective section befire executing it 
+
+int main()
+{
+    /*
+    printf("Objective_1\n");
+    cout<<"Hello MySirG";
+    */
+
+
+
+    /*
+    printf("Objective_2\n");
+    cout<<"Hello"<<endl<<"MySirG";
+    */
+
+    
+    /*
+    printf("Objective_3\n");
+    int a,b;
+    cout<<"Enter the values of two variables"<<endl;
+    cin>>a>>b;
+    cout<<"The sum of the entered two number is "<<(a+b);
+    */
+
+
+    /*
+    printf("Objective_4\n");
+    float r;
+    cout<<"Enter the radius of the circle"<<endl;
+    cin>>r;
+    cout<<"The area of the circle is "<<3.14*r*r;
+    */
+
+    
+    /*
+    printf("Objective_5\n");
+    float a,b,c;
+    cout<<"Enter the sides of the cuboid"<<endl;
+    cin>>a>>b>>c;
+    cout<<"The volume of the cuboid is "<<a*b*c;
+    */
+
+
+    /*
+    printf("Objective_6\n");
+    float a,b,c;
+    cout<<"Enter the three numbers"<<endl;
+    cin>>a>>b>>c;
+    cout<<"The average of the entered three is : "<<(a+b+c)/3;
+    */
+
+
+    /*
+    printf("Objective_7\n");
+    int a;
+    cout<<"Enter a number: "<<endl;
+    cin>>a;
+    cout<<"The square of the entered number is : "<<a*a;
+    */
+
+
+    /*
+    printf("Objective_8\n");
+    int a,b;
+    cout<<"Enter two numbers: "<<endl;
+    cin>>a>>b;
+    cout<<"The number before swapping are a:"<<a<<" and b:"<<b<<endl;
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    cout<<"The number after swapping are a:"<<a<<" and b:"<<b;
+    */
+
+
+
+    /*
+    printf("Objective_9\n");
+    int a,b;
+    cout<<"Enter two numbers: "<<endl;
+    cin>>a>>b;
+    if (a>b)
+    {
+        cout<<"The greatest number is "<<a;
+    }
+    else
+    {
+        cout<<"The greatest number is "<<b;
+    }
+    */
+
+
+    /*
+    printf("Objective_10\n");
+    int a[10] = {1,3,5,3,2,3,4,5,6,7},sum,i;
+    for (i=0;i<10;i++)
+    {
+        sum = sum+a[i];
+    }
+    cout<<"The sum of the elements of the entered array is: "<<sum;
+    */ 
+
+
+    return 0;
+}
